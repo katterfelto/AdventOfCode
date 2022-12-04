85467fc3f914f63cbf0c2682bae3faa7fd3e5185
Dave Moor <katterfelto@outlook.com>
2022-12-04 12:05:10 +0000

    Added 2022-04

---
+++ 2022/day-04.py
@@ -0,0 +1,42 @@
+import aocd
+from os.path import exists
+
+year = 2022
+day = 4
+
+######################################################
+# Get the Input data
+######################################################
+if not exists(f'./inputData/{year}/{day:02d}.txt'):
+    with open(f'./inputData/{year}/{day:02d}.txt', 'w') as f:
+        f.write(aocd.get_data(day=day, year=2022))
+with open(f'inputData/{year}/{day:02d}.txt', 'r') as f:
+    data = f.read().split('\n')
+
+######################################################
+# The solution
+######################################################
+result1 = 0
+result2 = 0
+
+import re
+
+for s in data:
+    l = len(s)
+    if l > 0:
+        pairs = re.split(',|-', s)
+        if (int(pairs[0]) >= int(pairs[2]) and int(pairs[1]) <= int(pairs[3])) or \
+            (int(pairs[2]) >= int(pairs[0]) and int(pairs[3]) <= int(pairs[1])) :
+            result1 += 1
+        if (int(pairs[0]) >= int(pairs[2]) and int(pairs[0]) <= int(pairs[3])) or \
+            (int(pairs[1]) >= int(pairs[2]) and int(pairs[1]) <= int(pairs[3])) or \
+            (int(pairs[2]) >= int(pairs[0]) and int(pairs[2]) <= int(pairs[1])) or \
+            (int(pairs[3]) >= int(pairs[0]) and int(pairs[3]) <= int(pairs[1])):
+            result2+= 1
+        
+######################################################
+# Output the results
+######################################################
+print(f'Results - {year}/{day:02d}:')
+print(f"Result 1 - {result1}")
+print(f"Result 2 - {result2}")
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ My solutions to the brilliant [Advent of Code](https://adventofcode.com/) create
 
 ## My setup
 
-I'm currenty woring in python and using the [advent-of-code-data](https://pypi.org/project/advent-of-code-data/) python module to access the input data. I'm using VS Code and executing the scripts from the root of the repo (hence the paths to arcive the input data files).
+I'm currenty working in python and using the [advent-of-code-data](https://pypi.org/project/advent-of-code-data/) python module to access the input data. I'm using VS Code and executing the scripts from the root of the repo (hence the paths to arcive the input data files).
